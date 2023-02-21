26a0513a8ca566adc5c24b8b1124ceedd586890a
geovannysan <geovanny1santiago@gmail.com>
2023-02-21 16:35:55 -0500

    agregando jwtoken

--- Program.cs
+++ Program.cs
@@ -2,10 +2,33 @@
 using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoftde.Datas;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//jswtoken
+builder.Configuration.AddJsonFile("appsettings.json");
+var secret = builder.Configuration.GetSection("settings").GetSection("secrekey").ToString();
+var keybayts = Encoding.UTF8.GetBytes(secret);
+builder.Services.AddAuthentication(conf =>
+{
+    conf.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    conf.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(keybayts),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+//
 
 builder.Services.AddControllers();
 
@@ -25,7 +48,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+//jswtoken
+app.UseAuthentication();
+//
 app.UseAuthorization();
 
 app.MapControllers();
